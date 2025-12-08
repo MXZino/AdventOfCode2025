fd563570162a62a802ec0f6e5d8531e899de9891
Bartosz Gulatowski <b.gulatowski@boosthigh.com>
2025-12-08 10:27:14 +0100

    Add solution for part two of day 02 puzzle

--- day_02.py
+++ day_02.py
@@ -32,12 +32,47 @@ def get_part_one(puzzle_data):
             num += 1
     print("Sum " + str(sum))
 
+def get_part_two(puzzle_data):
+    sum = 0
+    for row in puzzle_data:
+        num = int(row[0])
+        end = int(row[1])
+        while num <= end:
+            num_str = str(num)
+            str_len = len(num_str)
+            half = str_len // 2
+            for i in range(half):
+                ok = True
+                if str_len % (i +1) != 0:
+                    continue
+                substr = num_str[0 : i+1]
+                substr_len = len(substr)
+                compares = str_len // substr_len - 1
+                for comp in range(compares):
+                    start_idx = (comp+1) * substr_len
+                    end_idx = (comp+2) * substr_len
+                    to_compare = num_str[start_idx:end_idx]
+                    if substr != to_compare:
+                        ok = False
+                        break
+
+                if ok:
+                    sum += num
+                    break
+
+            num += 1
+
+    print("Sum " + str(sum))
+
 prepared_puzzle = prepare_puzzle("puzzles/examples/day_02.txt")
 print("Part one example:")
 get_part_one(prepared_puzzle)
+print("Part two example:")
+get_part_two(prepared_puzzle)
 
 prepared_puzzle = prepare_puzzle("puzzles/day_02.txt")
 print("Part one:")
 get_part_one(prepared_puzzle)
-
+print("Part two:")
+get_part_two(prepared_puzzle)
 print("Ok")
